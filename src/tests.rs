577831059c7b3eab9469e9b7334f5d3cd5648ef0
Johannes Hengstler <ubezl@student.kit.edu>
2024-10-06 20:41:43 +0200

    add test for relaxed fasta

--- src/tests.rs
+++ src/tests.rs
@@ -11,12 +11,16 @@ fn one_sequence() {
     let seq = r#"
 >P32234 1-368
 MSTILEKISAIESEMARTQKNKATSAHLGLLKAKLAKLRRELISPKGGGGGTGEAGFEVAKTGDARVGFVGFPSVGKSTL
-    "#.trim();
+    "#
+    .trim();
 
     let fasta = parse_fasta(&seq).expect("Failed to parse FASTA");
     assert_eq!(fasta.sequences.len(), 1);
     assert_eq!(fasta.sequences[0].description, b"P32234 1-368");
-    assert_eq!(String::from_utf8(fasta.sequences[0].iter().copied().collect::<Vec<_>>()).unwrap(), "MSTILEKISAIESEMARTQKNKATSAHLGLLKAKLAKLRRELISPKGGGGGTGEAGFEVAKTGDARVGFVGFPSVGKSTL");
+    assert_eq!(
+        String::from_utf8(fasta.sequences[0].iter().copied().collect::<Vec<_>>()).unwrap(),
+        "MSTILEKISAIESEMARTQKNKATSAHLGLLKAKLAKLRRELISPKGGGGGTGEAGFEVAKTGDARVGFVGFPSVGKSTL"
+    );
 }
 
 #[test]
@@ -27,14 +31,42 @@ MSTILEKISAIESEMARTQKNKATSAHLGLLKAKLAKLRRELISPKGGGGGTGEAGFEVAKTGDARVGFVGFPSVGKSTL
 
 >O77448 1-1117
 MQKINNINNNKQMLTRKEDLLTVLKQISALKYVSNLYEFLLATEKIVQTSELDTQFQEFLTTTIIASEQNLVENYKQKYN
-    ".trim();
+    "
+    .trim();
 
     let fasta = parse_fasta(&seq).expect("Failed to parse FASTA");
     assert_eq!(fasta.sequences.len(), 2);
 
     assert_eq!(fasta.sequences[0].description, b"P32234 1-368");
-    assert_eq!(String::from_utf8(fasta.sequences[0].iter().copied().collect::<Vec<_>>()).unwrap(), "MSTILEKISAIESEMARTQKNKATSAHLGLLKAKLAKLRRELISPKGGGGGTGEAGFEVAKTGDARVGFVGFPSVGKSTL");
+    assert_eq!(
+        String::from_utf8(fasta.sequences[0].iter().copied().collect::<Vec<_>>()).unwrap(),
+        "MSTILEKISAIESEMARTQKNKATSAHLGLLKAKLAKLRRELISPKGGGGGTGEAGFEVAKTGDARVGFVGFPSVGKSTL"
+    );
 
     assert_eq!(fasta.sequences[1].description, b"O77448 1-1117");
-    assert_eq!(String::from_utf8(fasta.sequences[1].iter().copied().collect::<Vec<_>>()).unwrap(), "MQKINNINNNKQMLTRKEDLLTVLKQISALKYVSNLYEFLLATEKIVQTSELDTQFQEFLTTTIIASEQNLVENYKQKYN");
+    assert_eq!(
+        String::from_utf8(fasta.sequences[1].iter().copied().collect::<Vec<_>>()).unwrap(),
+        "MQKINNINNNKQMLTRKEDLLTVLKQISALKYVSNLYEFLLATEKIVQTSELDTQFQEFLTTTIIASEQNLVENYKQKYN"
+    );
+}
+
+#[test]
+fn test_relaxed_fasta() {
+    let seq = r"
+>P32234 1-368
+MSTILEKISAIESEMARTQKNKATSAHLGLLKAKLAKLRRELISPKGGGGGTGEAGFEVAKTGDARVGFVGFPSVGKSTL
+MQKINNINNNKQMLTRKEDLLTVLKQISALKYVSNLYEFLLATEKIVQTSELDTQFQEFLTTTII
+
+ASEQNLVENYKQKYN
+
+
+    "
+    .trim();
+
+    let fasta = parse_fasta(&seq).expect("Failed to parse FASTA");
+    assert_eq!(fasta.sequences.len(), 1);
+
+    assert_eq!(fasta.sequences[0].description, b"P32234 1-368");
+    assert_eq!(String::from_utf8(fasta.sequences[0].iter().copied().collect::<Vec<_>>()).unwrap(),
+               "MSTILEKISAIESEMARTQKNKATSAHLGLLKAKLAKLRRELISPKGGGGGTGEAGFEVAKTGDARVGFVGFPSVGKSTLMQKINNINNNKQMLTRKEDLLTVLKQISALKYVSNLYEFLLATEKIVQTSELDTQFQEFLTTTIIASEQNLVENYKQKYN")
 }
